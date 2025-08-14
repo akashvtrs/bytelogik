315c25b1cf7c9766ec1a70f0fcb0298997db6135
AKASH M <110366863+akashvtrs@users.noreply.github.com>
2025-08-14 16:48:03 +0530

    Removes dependency on dart:io’s Platform, which is unavailable in the browser.

--- lib/services/databaseservice.dart
+++ lib/services/databaseservice.dart
@@ -3,7 +3,7 @@ import 'package:sembast/sembast_io.dart' show databaseFactoryIo;
 import 'package:sembast_web/sembast_web.dart' show databaseFactoryWeb;
 import 'package:path_provider/path_provider.dart';
 import 'package:path/path.dart';
-import 'dart:io' show Platform;
+import 'package:flutter/foundation.dart' show kIsWeb;
 
 class DatabaseService {
   static Database? _db;
@@ -11,12 +11,14 @@ class DatabaseService {
   Future<Database> get database async {
     if (_db != null) return _db!;
 
-    if (Platform.isAndroid || Platform.isIOS) {
+    if (kIsWeb) {
+      // Use the web database factory
+      _db = await databaseFactoryWeb.openDatabase('app.db');
+    } else {
+      // Use the IO database factory for mobile/desktop
       final dir = await getApplicationDocumentsDirectory();
       await dir.create(recursive: true);
       _db = await databaseFactoryIo.openDatabase(join(dir.path, 'app.db'));
-    } else {
-      _db = await databaseFactoryWeb.openDatabase('app.db');
     }
     return _db!;
   }
